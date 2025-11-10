f2e188003d72a75647248e9e1af216e52e99cd2a
Rahul Sahani <66432121+rahulsahani1137@users.noreply.github.com>
2025-11-10 08:11:52 +0530

    feat: add website assets, tailwind support and html boilerplate code

--- week-1/figma-site/tailwind.config.js
+++ week-1/figma-site/tailwind.config.js
@@ -1,8 +1,25 @@
- /** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
 export default {
-   content: ["./src/**/*.{html,js}"],
-   theme: {
-     extend: {},
-   },
-   plugins: [],
- }
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens: {
+      tablet: '800px',
+      desktop: '1280px'
+    }
+    ,
+    colors: {
+      'headline': '#000000',
+      'caption': '#485C11',
+      'paragraph': '#6F6F6F',
+      'divider': '#E9E9E9',
+      'accent-primary': '#485C11',
+      'accent-secondary': '#DFECC6'
+    },
+    fontFamily: {
+      "dm-sans": ['"DM Sans"', "sans-serif"],
+      "crimson-text": ['"Crimson Text"', "serif"],
+      "roboto-mono": ["Roboto Mono", "sans-serif"]
+    },
+  },
+  plugins: [],
+}
